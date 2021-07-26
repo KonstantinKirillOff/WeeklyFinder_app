9f7762afbea204293fa9226dc87e2e5466be0ec9
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-07-26 11:02:04 +0300

    add code to work labels and button

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -9,10 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var findDayButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        findDayButton.layer.cornerRadius = 10
+        
     }
+    
+    @IBAction func getDate(_ sender: UIButton) {
+        
+//        if let day = dateTF.text, let month = monthTF, let year = yearTF {
+//
+//        }
+        
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else {
+            return
+        }
+            
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekDay = dateFormatter.string(from: date)
+        
+        resultLabel.text = weekDay
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 
 
 }
